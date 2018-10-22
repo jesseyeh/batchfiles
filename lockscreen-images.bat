225ac96ad2ff057df86aaece5c186321c807fdae
Jesse Yeh <jesseyeh@users.noreply.github.com>
2018-10-21 21:08:07 -0700

    Set current directory as default output location

--- lockscreen-images.bat
+++ lockscreen-images.bat
@@ -1,15 +1,18 @@
 @echo off
 
-:: Copies Windows 10 lockscreen images into a specified directory
-if "%~1"=="" (
-    echo Specify destination file path.
-) else (
-    set DESTINATION="%~1\Images"
-    if not exist %DESTINATION% (
-        mkdir %DESTINATION%
-        echo Created directory at %DESTINATION%
-    )
-    xcopy /s C:\Users\%USERNAME%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\* %DESTINATION%
-    ren %DESTINATION%\* *.jpg
-    echo Finished copying lockscreen images to %DESTINATION%
+setlocal EnableDelayedExpansion
+
+:: Copies Windows 10 lockscreen images into a specified directory (current directory otherwise)
+set DESTINATION=.\Images
+if not "%~1"=="" (
+    set DESTINATION=%~1
+    if !DESTINATION:~-1!==\ set DESTINATION=!DESTINATION:~0,-1!
+    set DESTINATION=!DESTINATION!\Images
 )
+if not exist !DESTINATION! (
+    mkdir !DESTINATION!
+    echo Created directory at !DESTINATION!
+)
+xcopy /s C:\Users\%USERNAME%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\* !DESTINATION!
+ren !DESTINATION!\* *.jpg
+echo Finished copying lockscreen images to !DESTINATION!
